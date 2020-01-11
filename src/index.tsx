61ad23df64cd6f0ca82d016ebdb776ca501e99b4
michaelcjames10@gmail.com <michaelcjames10@gmail.com>
2020-01-11 20:49:59 +0000

    landing page

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,4 @@ import * as ReactDOM from "react-dom";
 import App from "./app/App";
 import "./index.css";
 
-import registerServiceWorker from "./registerServiceWorker";
-
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
