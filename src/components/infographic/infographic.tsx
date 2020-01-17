7215ac3af9bed20aff5d2580484872e6bda6052c
michaelcjames10@gmail.com <michaelcjames10@gmail.com>
2020-01-17 23:05:14 +0000

    landing work

--- src/components/infographic/infographic.tsx
+++ src/components/infographic/infographic.tsx
@@ -7,6 +7,7 @@ import styled from "styled-components";
 const InfographicStyle = styled.div`
   .infographic-block {
     background: white;
+    padding-bottom: 50px;
   }
   .infographic-block svg:nth-of-type(1) {
     transform: rotate(180deg) translateY(2px);
