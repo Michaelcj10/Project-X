ab8efc802acd4555f2e20b4c07aa5b113cf5b6ed
michael <michaelcjames10@gmail.com>
2020-12-30 14:40:56 +0000

    add email send with netlify

--- src/components/atoms/inputs/input.tsx
+++ src/components/atoms/inputs/input.tsx
@@ -24,6 +24,7 @@ const InputStyle = styled.div`
     border-right: none !important;
     border-bottom: 1px solid #fff;
     background: transparent;
+    color: #fff;
   }
   input:focus {
     outline: none;
